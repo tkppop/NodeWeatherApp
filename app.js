54ccd73f80088013da5fa8c287ef73156434900f
tkppop <ptk16@case.edu>
2019-03-10 21:24:36 -0400

    updates for geolocation for NodeWeatherApp

--- app.js
+++ app.js
@@ -9,8 +9,19 @@ const request = require('request')
 // 	console.log('The temperature is',response.body.currently.temperature)
 // })
 
-const mapurl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/New%20york.json?access_token=pk.eyJ1IjoicHRrMTYiLCJhIjoiY2p0M2t6Y3pwMHRtYTQ5bGxkamtrYnNpeiJ9.FPvR5kzUTigu74twJG-d-w'
+//const mapurl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/new%20york.json?access_token=pk.eyJ1IjoicHRrMTYiLCJhIjoiY2p0M2t6Y3pwMHRtYTQ5bGxkamtrYnNpeiJ9.FPvR5kzUTigu74twJG-d-w'
+const mapurl = 'https://api.mapbox.com/geocoding/v5/mapbox.places?access_token=pk.eyJ1IjoicHRrMTYiLCJhIjoiY2p0M2t6Y3pwMHRtYTQ5bGxkamtrYnNpeiJ9.FPvR5kzUTigu74twJG-d-w'
 
 request({url: mapurl, json:true},(error, response)=>{
-	console.log(response.body.features[0])
+	if(error){
+		console.log('Cannot connect to server')
+	} else if (response.body.message) {
+		console.log('Invalid request, check input ',response.message)
+	} else if (response.body.features.length == 0) {
+		console.log('Invalid location, check input ',response.message)
+	} else {
+		//console.log(response.body.features[0])
+		console.log('Location String',response.body.features[0].place_name)
+		console.log('Coordinates',response.body.features[0].center)
+	}
 })
